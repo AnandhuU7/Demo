6702b9fb90e8f76bdf5705a5c9f92e59ddd1673f
AnandhuU7 <anandhuachu777.com>
2024-06-13 14:32:52 +0530

    updated error msg

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const connectDB = async () => {
     await mongoose.connect(mongoDB);
     console.log("db connected");
   } catch (err) {
-    console.error("db connection error:", error);
+    console.error("db connection :", error);
   }
 };
 
